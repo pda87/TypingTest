138f61d7d47d6a82f2314539f6a638e10b43654a
Pete Arden <pete.davidarden@gmail.com>
2015-06-19 21:22:04 +0100

    TypingTest 19/06/2015
    Initial commit.

---
+++ TypingTest/TypingTest.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace TypingTest
+{
+    partial class TypingTest
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.roundTimer = new System.Windows.Forms.Timer(this.components);
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.wordLabel = new System.Windows.Forms.Label();
+            this.timeLabel = new System.Windows.Forms.Label();
+            this.gameLabel = new System.Windows.Forms.Label();
+            this.scoreLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 10;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // roundTimer
+            // 
+            this.roundTimer.Interval = 1000;
+            this.roundTimer.Tick += new System.EventHandler(this.roundTimer_Tick);
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inputTextBox.Location = new System.Drawing.Point(-5, 112);
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(590, 35);
+            this.inputTextBox.TabIndex = 0;
+            // 
+            // wordLabel
+            // 
+            this.wordLabel.AutoSize = true;
+            this.wordLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.wordLabel.Location = new System.Drawing.Point(189, 38);
+            this.wordLabel.Name = "wordLabel";
+            this.wordLabel.Size = new System.Drawing.Size(139, 31);
+            this.wordLabel.TabIndex = 1;
+            this.wordLabel.Text = "wordLabel";
+            // 
+            // timeLabel
+            // 
+            this.timeLabel.AutoSize = true;
+            this.timeLabel.Font = new System.Drawing.Font("Segoe Print", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timeLabel.Location = new System.Drawing.Point(12, 363);
+            this.timeLabel.Name = "timeLabel";
+            this.timeLabel.Size = new System.Drawing.Size(58, 84);
+            this.timeLabel.TabIndex = 2;
+            this.timeLabel.Text = "t";
+            // 
+            // gameLabel
+            // 
+            this.gameLabel.AutoSize = true;
+            this.gameLabel.Font = new System.Drawing.Font("Lucida Console", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gameLabel.Location = new System.Drawing.Point(165, 190);
+            this.gameLabel.Name = "gameLabel";
+            this.gameLabel.Size = new System.Drawing.Size(186, 32);
+            this.gameLabel.TabIndex = 3;
+            this.gameLabel.Text = "gameLabel";
+            // 
+            // scoreLabel
+            // 
+            this.scoreLabel.AutoSize = true;
+            this.scoreLabel.Font = new System.Drawing.Font("Microsoft NeoGothic", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scoreLabel.Location = new System.Drawing.Point(131, 265);
+            this.scoreLabel.Name = "scoreLabel";
+            this.scoreLabel.Size = new System.Drawing.Size(255, 64);
+            this.scoreLabel.TabIndex = 4;
+            this.scoreLabel.Text = "scoreLabel";
+            // 
+            // TypingTest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 561);
+            this.Controls.Add(this.scoreLabel);
+            this.Controls.Add(this.gameLabel);
+            this.Controls.Add(this.timeLabel);
+            this.Controls.Add(this.wordLabel);
+            this.Controls.Add(this.inputTextBox);
+            this.Name = "TypingTest";
+            this.Text = "Typing Test";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Timer roundTimer;
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.Label wordLabel;
+        private System.Windows.Forms.Label timeLabel;
+        private System.Windows.Forms.Label gameLabel;
+        private System.Windows.Forms.Label scoreLabel;
+    }
+}
+
---
+++ TypingTest/TypingTest.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TypingTest
+{
+    public partial class TypingTest : Form
+    {
+        string input = string.Empty;
+        string word = string.Empty;
+        int score = 0;
+        string[] words = File.ReadAllLines(@"words.txt");
+        Random random = new Random();
+        int gameTime = 60;
+        List<string> quickWords = new List<string>();
+
+        public TypingTest()
+        {
+            InitializeComponent();
+            wordLabel.Text = string.Empty;
+            wordLabel.TextAlign = ContentAlignment.MiddleCenter;
+            gameLabel.Text = string.Empty;
+            gameLabel.TextAlign = ContentAlignment.MiddleCenter;
+            timeLabel.Text = "Time:\r\n" + gameTime.ToString();
+            timeLabel.Refresh();
+            timeLabel.TextAlign = ContentAlignment.MiddleCenter;
+            inputTextBox.TextAlign = HorizontalAlignment.Center;
+            scoreLabel.Width = gameLabel.Width = inputTextBox.Width;
+            int randomWord = random.Next(1, words.Length - 1);
+            word = words[randomWord];
+            wordLabel.TextAlign = ContentAlignment.MiddleCenter;
+            wordLabel.Text = word;
+            wordLabel.Refresh();
+            quickWords = generateWords();
+            timer.Start();
+        }
+
+        private void roundTimer_Tick(object sender, EventArgs e)
+        {
+            gameTime--;
+            timeLabel.Text = "Time:\r\n" + gameTime.ToString();
+            timeLabel.Refresh();
+
+            if (gameTime == 0)
+            {
+                gameLabel.Text = "Game over!";
+                scoreLabel.Text = "Score: " + score + " WPM";
+                inputTextBox.Enabled = false;
+                timer.Stop();
+                roundTimer.Stop();
+                return;
+            }
+        }
+
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+
+            scoreLabel.Text = "Score: " + score.ToString();
+            scoreLabel.Refresh();
+
+            input = inputTextBox.Text;
+
+            if (string.IsNullOrEmpty(input))
+            {
+                return;
+            }
+
+            else
+            {
+                if (!roundTimer.Enabled)
+                {
+                    roundTimer.Enabled = true;
+                }
+
+                char[] inputChars = input.ToCharArray();
+
+                foreach (char character in inputChars)
+                {
+                    if (character == ' ')
+                    {
+
+
+                        if (input.Trim() == word)
+                        {
+                            score++;
+                        }
+
+                        inputTextBox.Clear();
+                        int randomWord = random.Next(1, words.Length - 1);
+                        word = words[randomWord];
+                        wordLabel.Text = word;
+                        wordLabel.Refresh();
+
+                    }
+                }
+
+            }
+        }
+
+        private List<string> generateWords()
+        {
+            List<string> wordsToDisplay = new List<string>();
+
+            int randomWord = random.Next(1, words.Length - 1);
+
+            wordsToDisplay.Add(words[randomWord]);
+            wordsToDisplay.Add(words[randomWord]);
+            wordsToDisplay.Add(words[randomWord]);
+            wordsToDisplay.Add(words[randomWord]);
+            wordsToDisplay.Add(words[randomWord]);
+
+            return wordsToDisplay;
+        }
+
+
+    }
+}
